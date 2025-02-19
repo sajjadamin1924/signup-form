8acc00f1f30c76e69246f8e2d04636437acb6bf3
Sajjad amin <sajjadamin1924@gmail.com>
2025-02-19 19:15:53 +0500

    code updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
 import "./App.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faFacebook,
-  faGoogle,
-  faApple,
-} from "@fortawesome/free-brands-svg-icons";
+
 import { useState } from "react";
 import { Eye, EyeOff } from "lucide-react";
 
@@ -29,48 +24,55 @@ function App() {
             </p>
 
             <label className="block text-gray-600 mb-1">Full Name</label>
-            <div className="flex space-x-4  relative">
-              <img src="name.png" alt="Student" className=" absolute" />
+            <div className="relative flex items-center w-full">
+              <img
+                src="name.png"
+                alt="Student"
+                className="absolute left-4 w-6 h-6"
+              />
               <input
                 type="text"
-                className="w-full p-3  border border-gray-600 rounded-3xl "
-                placeholder="Enter Your name"
+                className="w-full p-3 pl-12 border border-gray-600 rounded-3xl"
+                placeholder="Enter Your Name"
                 required
               />
             </div>
+
             <label className="block text-gray-600 mb-1">Email</label>
-            <div className="flex space-x-4  relative">
-              <img src="email.png" alt="Student" className="absolute" />
+            <div className="relative flex items-center w-full">
+              <img
+                src="download1.png"
+                alt="Student"
+                className="absolute left-4 w-6 h-6"
+              />
               <input
                 type="email"
-                className="w-full p-3 border border-gray-600 rounded-3xl"
+                className="w-full p-3 pl-12 border border-gray-600 rounded-3xl"
                 placeholder="Enter Your Email"
                 required
               />
             </div>
 
-            <div className="relative w-full">
-              <label className="block text-gray-600 mb-1">Password</label>
-              <div className="relative">
-                <img
-                  src="password.png"
-                  alt="Student"
-                  className=" rounded-lg absolute "
-                />
-                <input
-                  type={showPassword ? "text" : "password"}
-                  className="w-full p-3 border border-gray-600 rounded-3xl pr-10"
-                  placeholder="Enter Your Password"
-                  required
-                />
-                <button
-                  type="button"
-                  className="absolute inset-y-0 right-3 flex items-center text-gray-600 cursor-pointer"
-                  onClick={() => setShowPassword(!showPassword)}
-                >
-                  {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
-                </button>
-              </div>
+            <label className="block text-gray-600 mb-1">Password</label>
+            <div className="relative flex items-center w-full">
+              <img
+                src="password.png"
+                alt="Student"
+                className="absolute left-4 w-6 h-6"
+              />
+              <input
+                type={showPassword ? "text" : "password"}
+                className="w-full p-3 pl-12 border border-gray-600 rounded-3xl"
+                placeholder="Enter Your Password"
+                required
+              />
+              <button
+                type="button"
+                className="absolute inset-y-0 right-3 flex items-center text-gray-600 cursor-pointer"
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+              </button>
             </div>
 
             <button
